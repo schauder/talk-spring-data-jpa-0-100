c67dd838f9db065c1f30fc6ac5b4464a2e996cb4
Jens Schauder <jschauder@pivotal.io>
2019-05-10 16:03:19 +0200

    first save that doesn't actually save anything.

---
+++ example/src/main/java/example/jpa/Person.java
@@ -0,0 +1,56 @@
+/*
+ * Copyright 2019 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package example.jpa;
+
+import lombok.Data;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToOne;
+import javax.persistence.Version;
+
+/**
+ * @author Jens Schauder
+ */
+@Data
+@Entity
+public class Person {
+
+	Person() { }
+
+	Person(String firstName, Address address, Gender gender) {
+
+		this.firstName = firstName;
+		this.address = address;
+		this.gender = gender;
+	}
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	Long id;
+	@Version
+	Long version;
+
+	String firstName;
+
+	@OneToOne(cascade = CascadeType.ALL, orphanRemoval = true)
+	Address address;
+
+	Gender gender;
+}
--- example/src/test/java/example/jpa/JpaApplicationTests.java
+++ example/src/test/java/example/jpa/JpaApplicationTests.java
@@ -2,15 +2,32 @@ package example.jpa;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import javax.persistence.EntityManager;
+import javax.transaction.Transactional;
+
 @RunWith(SpringRunner.class)
 @SpringBootTest
+@Transactional
 public class JpaApplicationTests {
+
+	@Autowired
+	EntityManager em;
 
 	@Test
 	public void contextLoads() {
+
+		Person p = new Person(
+				"Jens",
+				new Address("38116", "Braunschweig", "Germany"),
+				Gender.MALE
+		);
+
+		em.persist(p);
+
 	}
 
 }
