9629afd3da17bb2fa291f4e26451a2952e15144c
Jens Schauder <jschauder@pivotal.io>
2019-05-13 08:35:20 +0200

    Demonstrating write-behind behaviour of the EntityManager and

--- example/src/test/java/example/jpa/JpaApplicationTests.java
+++ example/src/test/java/example/jpa/JpaApplicationTests.java
@@ -1,23 +1,30 @@
 package example.jpa;
 
+import org.assertj.core.api.AbstractIntegerAssert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import javax.persistence.EntityManager;
 import javax.transaction.Transactional;
 
+import static org.assertj.core.api.Assertions.*;
+
 @RunWith(SpringRunner.class)
 @SpringBootTest
-@Transactional
 public class JpaApplicationTests {
 
 	@Autowired
 	EntityManager em;
 
+	@Autowired
+	JdbcTemplate template;
+
 	@Test
+	@Transactional
 	public void saving() {
 
 		Person p = new Person(
@@ -28,21 +35,33 @@ public class JpaApplicationTests {
 
 		em.persist(p);
 
+		Person reloaded = em.find(Person.class, p.getId());
+
+		assertThat(reloaded).isNotNull();
+		assertThat(reloaded.getId()).isEqualTo(p.getId());
+
+		assertPersonCount().isEqualTo(0);
+
+		Person reloadedAgain = em
+				.createQuery("SELECT p  FROM Person p WHERE p.id = :id", Person.class)
+				.setParameter("id", p.getId())
+				.getSingleResult();
+
+		assertPersonCount().isEqualTo(1);
+
+		// em.flush();
+
+		assertThat(p)
+				.isSameAs(reloaded)
+				.isSameAs(reloadedAgain);
+
 	}
 
+	private AbstractIntegerAssert<?> assertPersonCount() {
 
-	@Test
-	public void savingForReal() {
-
-		Person p = new Person(
-				"Jens",
-				new Address("38116", "Braunschweig", "Germany"),
-				Gender.MALE
+		return assertThat(
+				template.queryForObject("SELECT COUNT(*) FROM PERSON", Integer.class)
 		);
-
-		em.persist(p);
-		em.flush();
-
 	}
 
 
